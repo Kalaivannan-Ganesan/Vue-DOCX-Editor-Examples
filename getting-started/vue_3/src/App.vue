824f8103b9243b40195f8a9e97b5c0f4ffd3a5ce
Kalaivannan-Ganesan <93248069+Kalaivannan-Ganesan@users.noreply.github.com>
2026-05-21 10:30:51 +0530

    Added Vue DOCX Editor samples

---
+++ getting-started/vue_3/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ejs-documenteditorcontainer 
+   height="590px"
+   :serviceUrl="serviceUrl" 
+   :enableToolbar="true"> 
+  </ejs-documenteditorcontainer>
+</template>
+
+<script setup>
+  import { provide } from 'vue';
+  import { DocumentEditorContainerComponent as EjsDocumenteditorcontainer, Toolbar } from '@syncfusion/ej2-vue-documenteditor';
+  
+  // Use the following service URL only for demo purposes
+  const serviceUrl = 'https://document.syncfusion.com/web-services/docx-editor/api/documenteditor/';
+
+  provide('DocumentEditorContainer', [Toolbar]);
+</script>
+
+<style>
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-inputs/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-popups/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-lists/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-navigations/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-splitbuttons/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-dropdowns/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-vue-documenteditor/styles/material.css';
+</style>
